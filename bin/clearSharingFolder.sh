9932c67af70bbe5901055aca913f2281c808779e
Stefan Zweifel <hello@stefanzweifel.io>
2015-01-02 21:31:48 +0100

    Added clearSharingFolder.sh
    Remove all files from a given folder, older than 24 hours. Works nice with http://getdropsha.re

--- bin/clearSharingFolder.sh
+++ bin/clearSharingFolder.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
 # Clear Sharing folder
+# Remove all files older than 24 hours
 #
 
 $FOLDER_PATH=PATH_TO_SHARING_FOLDER
 
-/bin/find $FOLDER_PATH -maxdepth 1 -mindepth 1 -type d -mmin +1440 -exec rm -rf {} \;
+find $FOLDER_PATH -maxdepth 1 -mindepth 1 -type d -mmin +1440 -exec rm -rf {} \;
